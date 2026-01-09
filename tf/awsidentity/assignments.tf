63bfbc0ce987e56e545faf1b03945db75fc05e07
parikshit <parixit23@gmail.com>
2026-01-10 01:48:37 +0900

    tf : poweruser permission set and assignment

--- tf/awsidentity/assignments.tf
+++ tf/awsidentity/assignments.tf
@@ -9,6 +9,12 @@ locals {
     dev  = local.account_ids.dev
     prod = local.account_ids.prod
   }
+  admin_accounts = {
+    prod = local.account_ids.prod
+  }
+  poweruser_accounts = {
+    dev = local.account_ids.dev
+  }
 }
 
 resource "aws_ssoadmin_account_assignment" "tfreadonly" {
@@ -23,3 +29,30 @@ resource "aws_ssoadmin_account_assignment" "tfreadonly" {
   target_id   = each.value
   target_type = "AWS_ACCOUNT"
 }
+
+resource "aws_ssoadmin_account_assignment" "tfadmin" {
+  for_each = local.admin_accounts
+
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.tfadmin.arn
+
+  principal_id   = aws_identitystore_group.break_glass.group_id
+  principal_type = "GROUP"
+
+  target_id   = each.value
+  target_type = "AWS_ACCOUNT"
+}
+
+resource "aws_ssoadmin_account_assignment" "tfpoweruser" {
+  for_each = local.poweruser_accounts
+
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.tfpoweruser.arn
+
+  principal_id   = aws_identitystore_group.tf_admins.group_id
+  principal_type = "GROUP"
+
+  target_id   = each.value
+  target_type = "AWS_ACCOUNT"
+}
+
--- tf/awsidentity/permission_sets.tf
+++ tf/awsidentity/permission_sets.tf
@@ -25,3 +25,17 @@ resource "aws_ssoadmin_managed_policy_attachment" "readonly_policy" {
   permission_set_arn = aws_ssoadmin_permission_set.tfreadonly.arn
   managed_policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
+
+resource "aws_ssoadmin_permission_set" "tfpoweruser" {
+  name         = "tfPowerUser"
+  description  = "PowerUser access for dev accounts"
+  instance_arn = local.sso_instance_arn
+
+  session_duration = "PT8H"
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "poweruser_policy" {
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.tfpoweruser.arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
