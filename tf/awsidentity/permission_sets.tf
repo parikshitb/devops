382e02a0a36a87ccf5b623f822aeacec3dd1ff2a
parikshit <parixit23@gmail.com>
2026-01-10 00:46:42 +0900

    tf : permission sets added

---
+++ tf/awsidentity/permission_sets.tf
@@ -0,0 +1,27 @@
+resource "aws_ssoadmin_permission_set" "tfadmin" {
+  name         = "tfAdmin"
+  description  = "Administrator access"
+  instance_arn = local.sso_instance_arn
+
+  session_duration = "PT8H"
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "admin_policy" {
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.tfadmin.arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_ssoadmin_permission_set" "tfreadonly" {
+  name         = "tfReadOnly"
+  description  = "Read-only access"
+  instance_arn = local.sso_instance_arn
+
+  session_duration = "PT8H"
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "readonly_policy" {
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.tfreadonly.arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
